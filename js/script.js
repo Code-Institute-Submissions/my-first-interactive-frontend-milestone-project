fda35611de44ec8b96e033c5cdc97ca77fbf34e2
Eliana Beckinsale <Elianageral@hotmail.com>
2020-06-08 02:36:03 +0000

    removed Google map api code from events.html as it was not needed. Added images to Rooms.html and added Modal functionality to those images using Javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,52 @@
+function waitForLoad(id, callback){
+    var timer = setInterval(function(){
+        if(document.getElementById(id)){
+            clearInterval(timer);
+            callback();
+        }
+    }, 100);
+}
+
+waitForLoad("modal", function(){
+    console.log("load successful, you can proceed!!");
+
+var modal = document.getElementById("modal");
+var roomImg = document.getElementById("myImg");
+var roomImg2 = document.getElementById("myImg2");
+var roomImg3 = document.getElementById("myImg3");
+var roomImg4 = document.getElementById("myImg4");
+var roomdesc1 = document.getElementById("room-desc-1");
+var room = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+  roomImg.onclick = function(){
+  modal.style.display = "block";
+  room.src = this.src;
+  captionText.innerHTML = this.alt;
+  }
+  roomImg2.onclick = function(){
+  modal.style.display = "block";
+  room.src = this.src;
+  }
+  roomImg3.onclick = function(){
+  modal.style.display = "block";
+  room.src = this.src;
+  }
+  roomImg4.onclick = function(){
+  modal.style.display = "block";
+  room.src = this.src;
+  }  
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+});
+
+
 var slideIndex = 0;
 showSlidesAuto(slideIndex);
 
@@ -10,5 +59,6 @@ function showSlidesAuto(n) {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlidesAuto, 4000); // Change image every 4 seconds
+  setTimeout(showSlidesAuto, 4000); 
 }
+
