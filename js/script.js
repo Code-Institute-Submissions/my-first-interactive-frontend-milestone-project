12b1e00f3b2ecfad9037b261a15145edf96e005a
Eliana Beckinsale <Elianageral@hotmail.com>
2020-06-26 23:44:19 +0000

    added id slideshow to the div that encloses the banner images. Also, made syntax corrections to script.js and added a second run of Waitforload function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
+// Hello.
+//
+// This is JSHint, a tool that helps to detect errors and potential
+// problems in your JavaScript code.
+//
+// To start, simply enter some JavaScript anywhere on this page. Your
+// report will appear on the right side.
+//
+// Additionally, you can toggle specific options in the Configure
+// menu.
+
 function waitForLoad(id, callback){
     var timer = setInterval(function(){
         if(document.getElementById(id)){
             clearInterval(timer);
             callback();
         }
-    }, 100);
+    },100);
 }
 
 waitForLoad("modal", function(){
     console.log("load successful, you can proceed!!");
+
 
 var modal = document.getElementById("modal");
 var roomImg = document.getElementById("myImg");
@@ -20,7 +32,6 @@ var roomImg6 = document.getElementById("myImg6");
 var roomImg7 = document.getElementById("myImg7");
 var roomImg8 = document.getElementById("myImg8");
 var roomImg9 = document.getElementById("myImg9");
-var roomdesc1 = document.getElementById("room-desc-1");
 var room = document.getElementById("img01");
 var imgLayer1 = document.getElementById("layer1");
 var imgLayer2 = document.getElementById("layer2");
@@ -33,41 +44,41 @@ var imgLayer8 = document.getElementById("layer8");
 var imgLayer9 = document.getElementById("layer9");
 
   imgLayer1.onclick = function(){
-  modal.style.display = "block";
-  room.src = roomImg.src;
-  }
+    modal.style.display = "block";
+    room.src = roomImg.src;
+  };
   imgLayer2.onclick = function(){
-  modal.style.display = "block";
-  room.src = roomImg2.src;
-  }
+    modal.style.display = "block";
+    room.src = roomImg2.src;
+  };
   imgLayer3.onclick = function(){
-  modal.style.display = "block";
-  room.src = roomImg3.src;
-  }
+    modal.style.display = "block";
+    room.src = roomImg3.src;
+  };
   imgLayer4.onclick = function(){
-  modal.style.display = "block";
-  room.src = roomImg4.src;
-  }
+    modal.style.display = "block";
+    room.src = roomImg4.src;
+  };
   imgLayer5.onclick = function(){
-  modal.style.display = "block";
-  room.src = roomImg5.src;
-  } 
+    modal.style.display = "block";
+    room.src = roomImg5.src;
+  };
   imgLayer6.onclick = function(){
-  modal.style.display = "block";
-  room.src = roomImg6.src;
-  } 
+    modal.style.display = "block";
+    room.src = roomImg6.src;
+  };
   imgLayer7.onclick = function(){
-  modal.style.display = "block";
-  room.src = roomImg7.src;
-  } 
+    modal.style.display = "block";
+    room.src = roomImg7.src;
+  };
   imgLayer8.onclick = function(){
-  modal.style.display = "block";
-  room.src = roomImg8.src;
-  } 
+    modal.style.display = "block";
+    room.src = roomImg8.src;
+  };
   imgLayer9.onclick = function(){
-  modal.style.display = "block";
-  room.src = roomImg9.src;
-  }  
+    modal.style.display = "block";
+    room.src = roomImg9.src;
+  };
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -75,13 +86,16 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
+};
 });
 
-/* Slideshow banner functionality on index.html */
-/* This code was partially taken from W3 Schools' website examples */
+// Slideshow banner functionality on index.html
+// This code was partially taken from W3 Schools' website examples
+
+waitForLoad("slideshow", function(){
+    console.log("load successful, you can proceed!!");
+
 var slideIndex = 0;
-showSlidesAuto(slideIndex);
 
 function showSlidesAuto(n) {
   var i;
@@ -90,7 +104,10 @@ function showSlidesAuto(n) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex > slides.length) {slideIndex = 1;}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlidesAuto,4000); 
+  setTimeout(showSlidesAuto, 4000);
 }
+
+showSlidesAuto(slideIndex);
+});
